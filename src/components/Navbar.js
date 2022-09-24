fd6c23008b1b21e3492e7e1f4d58b483d85cc95e
Drew Zinck <dlzincktheii@gmail.com>
2022-09-24 01:37:34 -0700

    added Hero component and began Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Away
-            <i class="far fa-paper-plane"></i>
+            <i className="far fa-paper-plane"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
