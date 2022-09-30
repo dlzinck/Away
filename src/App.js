bddef728b87f83c91b2dbbbdb35b6ad3e5dd8971
Drew Zinck <dlzincktheii@gmail.com>
2022-09-29 19:59:08 -0700

    syntax changes and hero img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
